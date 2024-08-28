57346e06498be13538a1bce6ec4c4ddd45a09156
John Browning <john@shadowman.jeb.dev>
2024-08-28 09:56:40 -0400

    ansible playbook now generates csv

--- vminfo/vminfo.py
+++ vminfo/vminfo.py
@@ -1,11 +1,11 @@
 import yaml
 
-with open('vmlist.yml', 'r') as file:
+with open('vmdata.yml', 'r') as file:
     vmdata = yaml.safe_load_all(file)
-    print("VMNAME , CORES , MEMORY") 
+    print("NAMESPACE, VMNAME , CORES , MEMORY") 
     for data in vmdata:
         for resource in data['resources']:
             if("memory" in resource['spec']['template']['spec']['domain']):
-                print(resource['metadata']['name'],",",resource['spec']['template']['spec']['domain']['cpu']['cores'],",",resource['spec']['template']['spec']['domain']['memory']['guest'])
+                print(resource['metadata']['namespace'],",",resource['metadata']['name'],",",resource['spec']['template']['spec']['domain']['cpu']['cores'],",",resource['spec']['template']['spec']['domain']['memory']['guest'])
             if("requests" in resource['spec']['template']['spec']['domain']['resources']):
-                print(resource['metadata']['name'],",",resource['spec']['template']['spec']['domain']['cpu']['cores'],",",resource['spec']['template']['spec']['domain']['resources']['requests']['memory'])
+                print(resource['metadata']['namespace'],",",resource['metadata']['name'],",",resource['spec']['template']['spec']['domain']['cpu']['cores'],",",resource['spec']['template']['spec']['domain']['resources']['requests']['memory'])
